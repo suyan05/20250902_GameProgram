cb6c0eaea41960ddc4d9998c0f0d15c3d592b940
suyan05 <cadhppr58@gmail.com>
2025-09-09 14:05:28 +0900

    Jump 및 Rotate 추가

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -5,26 +5,51 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     public float Speed = 5f;
+    public float JP = 4.5f;
+    public float Gv = 9.8f;
+    public float RS = 4.0f;
 
-
-    private CharacterController characterController;
+    private float h, v;
+    private Vector3 moveDir;
+    private CharacterController CC;
+    private float verticalVelocity = 0f;
 
     private void Start()
     {
-        characterController = GetComponent<CharacterController>();
+        CC = GetComponent<CharacterController>();
     }
 
     private void Update()
     {
-        if (characterController != null)
-        {
+        h = Input.GetAxis("Horizontal");
+        v = Input.GetAxis("Vertical");
 
+        Vector3 inputDir = new Vector3(h, 0, v);
+        inputDir = transform.TransformDirection(inputDir);
+
+        if (CC.isGrounded)
+        {
+            verticalVelocity = -1f;
+            if (Input.GetKeyDown(KeyCode.Space))
+            {
+                verticalVelocity = JP;
+            }
+        }
+        else
+        {
+            verticalVelocity -= Gv * Time.deltaTime; // 중력 적용
         }
 
-        float h = Input.GetAxis("Horizontal");
-        float v = Input.GetAxis("Vertical");
+        moveDir = inputDir * Speed;
+        moveDir.y = verticalVelocity;
 
-        Vector3 move = new Vector3(h, 0, v);
-        characterController.Move(move * Speed * Time.deltaTime);
+        CC.Move(moveDir * Time.deltaTime);
+
+        // 회전 처리
+        if (inputDir.magnitude > 0.1f)
+        {
+            Quaternion targetRotation = Quaternion.LookRotation(inputDir);
+            transform.rotation = Quaternion.Lerp(transform.rotation, targetRotation, Time.deltaTime * RS);
+        }
     }
 }
